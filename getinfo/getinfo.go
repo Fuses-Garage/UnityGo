261503776fc6c012d71af92eb4c4561ad03757fe
Fuses-Garage <1219mamemame@gmail.com>
2022-11-27 21:11:59 +0900

    Update getinfo.go

--- getinfo/getinfo.go
+++ getinfo/getinfo.go
@@ -1,15 +1,15 @@
 package getinfo
 
 import (
-	"database/sql"//SQL操作
-	"encoding/json"//json相互変換
-	"fmt"      //基本的な入出力処理
-	"io"       //入出力
-	"net/http" //http通信
+	"database/sql"  //SQL操作
+	"encoding/json" //json相互変換
+	"fmt"           //基本的な入出力処理
+	"io"            //入出力
+	"net/http"      //http通信
 	"os"
 
-	"github.com/joho/godotenv"//.envの使用
-	_ "github.com/lib/pq"//ポスグレ使用
+	"github.com/joho/godotenv" //.envの使用
+	_ "github.com/lib/pq"      //ポスグレ使用
 )
 
 func GetInfo(w http.ResponseWriter, r *http.Request) {
@@ -20,10 +20,10 @@ func GetInfo(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Printf("読み込み出来ませんでした: %v", err)
 	}
-	db, err := sql.Open("postgres", "user="+os.Getenv("ENVUSER")+" password="+os.Getenv("ENVPASS")+" dbname=UniGoDB sslmode=disable")//DBに接続
+	db, err := sql.Open("postgres", "user="+os.Getenv("ENVUSER")+" password="+os.Getenv("ENVPASS")+" dbname=UniGoDB sslmode=disable") //DBに接続
 	checkErr(err)
 	//データの検索
-	type idata struct {//レコード用の構造体
+	type idata struct { //レコード用の構造体
 		ID    int
 		TITLE string
 		ABOUT string
@@ -33,7 +33,7 @@ func GetInfo(w http.ResponseWriter, r *http.Request) {
 	var title string
 	var about string
 	var date string
-	var datarows []idata//データを格納する配列
+	var datarows []idata                               //データを格納する配列
 	rows, err := db.Query("SELECT * FROM public.info") //全取得
 	for rows.Next() {                                  //1つずつ処理
 		switch err := rows.Scan(&id, &title, &about, &date); err { //エラーの有無でスイッチ
