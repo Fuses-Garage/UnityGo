ed0e8909f8b96add48bb80b938a66ca584a2dd05
Fuses-Garage <1219mamemame@gmail.com>
2022-12-02 22:07:38 +0900

    ユーザー追加処理完成

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"github.com/Fuses-Garage/UnityGo/getinfo"
-	useradd "github.com/Fuses-Garage/UnityGo/usermethod"
+	"github.com/Fuses-Garage/UnityGo/usermethod"
 
 	//"log"      //エラーを表示するときに使います
 	"net/http" //HTTPを使った通信に必要
 )
 
 func main() {
-	http.HandleFunc("/", getinfo.GetInfo)        //ルートのアクセスにHelloWorldをハンドリング
-	http.HandleFunc("/adduser", useradd.UserAdd) //ルートのアクセスにHelloWorldをハンドリング
-	err := http.ListenAndServe(":80", nil)       //サーバ起動
+	http.HandleFunc("/getinfo", getinfo.GetInfo)    //ルートのアクセスにHelloWorldをハンドリング
+	http.HandleFunc("/useradd", usermethod.UserAdd) //ルートのアクセスにHelloWorldをハンドリング
+	err := http.ListenAndServe(":80", nil)          //サーバ起動
 	if err != nil {
 		//log.Fatal("ListenAndServe:", err)
 	}
--- usermethod/useradd.go
+++ usermethod/useradd.go
@@ -1,4 +1,4 @@
-package useradd
+package usermethod
 
 import (
 	"crypto/sha256" //ハッシュ関数
@@ -14,13 +14,21 @@ import (
 )
 
 func UserAdd(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("useradd")
 	if r.Method != "POST" { //一つでも空欄があれば
 		io.WriteString(w, "危険なのでPOSTメソッドでアクセスしてください。") //エラーメッセージを返す
 		return
 	}
 	err := r.ParseForm() //postボディを詠む
+	// フォームデータの取得
+	for k, v := range r.Form {
+		fmt.Printf("%s=%s\n", k, v)
+	}
 	checkErr(err)
-	if r.Form.Get("name") == "" || r.Form.Get("pass") == "" || r.Form.Get("loginname") == "" { //一つでも空欄があれば
+	fmt.Println(r.FormValue("name"))
+	fmt.Println(r.FormValue("pass"))
+	fmt.Println(r.FormValue("loginname"))
+	if r.FormValue("name") == "" || r.FormValue("pass") == "" || r.FormValue("loginname") == "" { //一つでも空欄があれば
 		io.WriteString(w, "全てのテキストボックスに値を入力してください。") //エラーメッセージを返す
 		return
 	}
@@ -31,7 +39,7 @@ func UserAdd(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Printf("読み込み失敗: %v", err)
 	}
-	row := db.QueryRow("SELECT COUNT(*) AS count FROM userdata_login WHERE loginname=$1 OR name=$2", r.Form.Get("loginname"), r.Form.Get("name")) //同じログインネームのユーザーを探す
+	row := db.QueryRow("SELECT COUNT(*) AS count FROM userdata_login WHERE loginname=$1 OR name=$2", r.FormValue("loginname"), r.FormValue("name")) //同じログインネームのユーザーを探す
 	var count int
 	err = row.Scan(&count) //ヒット数をintで受ける
 	checkErr((err))
@@ -39,15 +47,17 @@ func UserAdd(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "そのユーザーネームは既に登録されています") //エラーメッセージを返す
 		return
 	}
-	byt := sha256.Sum256([]byte(r.Form.Get("pass")))                                                                           //ハッシュ化
-	bina := byt[:]                                                                                                             //おまじない
-	hash := hex.EncodeToString(bina)                                                                                           //16進数の文字列に変化
-	_, err = db.Exec("INSERT INTO userdata_login VALUES(DEFAULT,$1,$2,$3)", r.Form.Get("name"), r.Form.Get("loginname"), hash) //新しいレコードを追加
+	byt := sha256.Sum256([]byte(r.FormValue("pass")))                                                                            //ハッシュ化
+	bina := byt[:]                                                                                                               //おまじない
+	hash := hex.EncodeToString(bina)                                                                                             //16進数の文字列に変化
+	_, err = db.Exec("INSERT INTO userdata_login VALUES(DEFAULT,$1,$2,$3)", r.FormValue("name"), r.FormValue("loginname"), hash) //新しいレコードを追加
 	checkErr(err)
 	io.WriteString(w, "success") //string化したものを送信
 }
 func checkErr(err error) {
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
+
 }
